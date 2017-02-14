a535be54b33c58e778a6e643e9caa3d48d04070f
hui793114465 <hui793114465@163.com>
2017-02-14 09:28:26 +0800

    modified constant

--- acg12/app/src/main/java/org/acg12/config/Constant.java
+++ acg12/app/src/main/java/org/acg12/config/Constant.java
@@ -13,8 +13,8 @@ public class Constant {
 	public static final int TOOLBAR_ID = -1;
 	public static final int LIMIT_PAGER = 20;
 
-	// url常量
-	public static final String URL = Constant.debug ? "http://192.168.1.102:8080/acg12-api/" : "http://139.196.46.40:8080/api/" ;
+	// url常量 http://192.168.1.107:8080/acg12-api/
+	public static final String URL = Constant.debug ? "http://139.196.46.40:8080/api/" : "http://139.196.46.40:8080/api-v2/" ;
 
 	// 申请权限
 	public static final int PERMISSION_CAMERE = 301;
