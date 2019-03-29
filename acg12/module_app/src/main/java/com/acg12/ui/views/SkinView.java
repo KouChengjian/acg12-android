7016f7cfa070c6ae13611325a9d0573bd8c6bc9d
kouchengjian <hui793114465@163.com>
2019-03-29 15:34:01 +0800

    feat:调试主题切换

--- acg12/module_app/src/main/java/com/acg12/ui/views/SkinView.java
+++ acg12/module_app/src/main/java/com/acg12/ui/views/SkinView.java
@@ -2,14 +2,14 @@ package com.acg12.ui.views;
 
 import android.support.v7.widget.Toolbar;
 
-import com.acg12.lib.utils.skin.entity.Skin;
-import com.acg12.lib.ui.base.ViewImpl;
+import com.acg12.R;
 import com.acg12.lib.ui.base.PresenterHelper;
+import com.acg12.lib.ui.base.ViewImpl;
+import com.acg12.lib.utils.skin.entity.Skin;
+import com.acg12.lib.widget.ToolBarView;
 import com.acg12.lib.widget.recycle.CommonRecycleview;
 import com.acg12.ui.adapter.SkinLoaderAdapter;
-
-import com.acg12.R;
-import com.acg12.ui.adapter.SkinLoaderAdapter;
+import com.google.android.exoplayer.C;
 
 import butterknife.BindView;
 
@@ -18,8 +18,8 @@ import butterknife.BindView;
  */
 public class SkinView extends ViewImpl {
 
-    @BindView(R.id.toolbar)
-    Toolbar toolbar;
+    @BindView(R.id.toolBarView)
+    ToolBarView toolBarView;
     @BindView(R.id.common_recyclerview)
     CommonRecycleview commonRecycleview;
     SkinLoaderAdapter skinLoaderAdapter;
@@ -32,11 +32,11 @@ public class SkinView extends ViewImpl {
     @Override
     public void created() {
         super.created();
-        toolbar.setNavigationIcon(R.mipmap.ic_action_back);
-        toolbar.setTitle(getContext().getString(R.string.skin));
+        toolBarView.setNavigationOrBreak("主题");
 
         commonRecycleview.setLinearLayoutManager();
         commonRecycleview.setLoadingEnabled(false);
+        commonRecycleview.setRefreshEnabled(false);
         skinLoaderAdapter = new SkinLoaderAdapter(getContext());
         commonRecycleview.setAdapter(skinLoaderAdapter);
     }
@@ -44,14 +44,18 @@ public class SkinView extends ViewImpl {
     @Override
     public void bindEvent() {
         super.bindEvent();
-        PresenterHelper.click(mPresenter , toolbar);
+        PresenterHelper.click(mPresenter, toolBarView.getToolbar());
     }
 
-    public void bindData(){
-        skinLoaderAdapter.add(new Skin(0xffD95555 , "姨妈红" , "default"));
-        skinLoaderAdapter.add(new Skin(0xfffb7299 , "少女粉" , "skin_pink.skin"));
-        skinLoaderAdapter.add(new Skin(0xff3998e1 , "胖次蓝" , "skin_blue.skin"));
-        skinLoaderAdapter.notifyDataSetChanged();
+    public ToolBarView getToolBarView() {
+        return toolBarView;
+    }
 
+    public void bindData() {
+        skinLoaderAdapter.add(new Skin(0xffD95555, "姨妈红", "default"));
+        skinLoaderAdapter.add(new Skin(0xfffb7299, "少女粉", "skin_pink.skin"));
+        skinLoaderAdapter.add(new Skin(0xff3998e1, "胖次蓝", "skin_blue.skin"));
+//        skinLoaderAdapter.add(new Skin(0xff46D29C, "胖次蓝", "skin_blue.skin"));
+        commonRecycleview.notifyChanged();
     }
 }
